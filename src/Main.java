18034615f4bd5fd3e06799f0efae0a27ae9eb55e
Sadig <sadigaxund@gmail.com>
2021-11-22 06:31:49 +0400

    + Added Blurring

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-
 import javax.imageio.ImageIO;
 
 /***************************************************************************
@@ -36,27 +37,70 @@ public class Main {
     static int size;
     static boolean isMultiThreaded;
 
-    public static void main(String[] args) {
-	if (args[0].equals("help")) {
-	    printHelp();
-	}
-
+    public static void main(String[] args) throws IOException {
 	try {
-	    processImage(args[1]);
-	    processSize(args[2]);
-	    processMode(args[3]);
+	    if (args[0].equals("help"))
+		printHelp();
+
+	    processImage(args[0]);
+	    processSize(args[1]);
+	    processMode(args[2]);
 	} catch (ArrayIndexOutOfBoundsException e) {
 	    printError(); // TODO: Maybe specify: Not enough arguments
 	}
-	
+
 	boxBlur();
-    }	
+    }
 
     static void boxBlur() {
-	
+
+	/* remove remainder from division and resize */
+	int width = size * (image.getWidth() / size);
+	int height = size * (image.getHeight() / size);
+	image = resize(image, width, height);
+
+	/* procces the image */
+	for (int row = 0; row < width; row++)
+	    for (int col = 0; col < height; col += size) {
+		verticalBlur(row, col);
+	    }
+
+	for (int col = 0; col < height; col++)
+	    for (int row = 0; row < width; row += size) {
+		horizontalBlur(row, col);
+	    }
+
+	/* write the image */
+	File file = new File("./blurredImage.png");
+
+	try {
+	    ImageIO.write(image, "png", file);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+    }
+
+    static void horizontalBlur(int xPixel, int yPixel) {
+	long sum = 0;
+	for (int i = xPixel; i < xPixel + size; i++)
+	    sum += image.getRGB(i, yPixel);
+	sum /= size;
+	for (int i = xPixel; i < xPixel + size; i++)
+	    image.setRGB(i, yPixel, (int) sum);
+    }
+
+    static void verticalBlur(int xPixel, int yPixel) {
+	long sum = 0;
+	for (int i = yPixel; i < yPixel + size; i++)
+	    sum += image.getRGB(xPixel, i);
+	sum /= size;
+	for (int i = yPixel; i < yPixel + size; i++)
+	    image.setRGB(xPixel, i, (int) sum);
     }
 
     static void processMode(String args) {
+
 	switch (args) {
 	case "S":
 	    isMultiThreaded = false;
@@ -65,6 +109,7 @@ public class Main {
 	    isMultiThreaded = true;
 	    break;
 	default:
+	    System.out.println("MODE!!!");
 	    printError(); // TODO: Maybe specify: Unidentified mode type
 	    break;
 	}
@@ -74,14 +119,16 @@ public class Main {
 	try {
 	    size = Integer.parseInt(args);
 	} catch (NumberFormatException nfe) {
+	    System.out.println("SIZE!!!");
 	    printError(); // TODO: Maybe specify: Numeric error
 	}
     }
 
     static void processImage(String args) {
 	try {
-	    image = ImageIO.read(URI.create(args).toURL());
+	    image = ImageIO.read(new File("./" + args));
 	} catch (IOException e) {
+	    System.out.println("IMAGE!!!:" + args);
 	    printError(); // TODO: Maybe specify: IO error
 	}
     }
@@ -91,7 +138,20 @@ public class Main {
     }
 
     static void printError() {
-
+	System.out.println("ERROR!!!");
 	System.exit(0);
+    }
+
+    // AUX METHODS
+
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+	Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+	BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+	Graphics2D g2d = dimg.createGraphics();
+	g2d.drawImage(tmp, 0, 0, null);
+	g2d.dispose();
+
+	return dimg;
     }
 }
